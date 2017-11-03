aa463f152907a96239b6186fb45ba39d4b464970
Stephen Gragg <graggsd@uab.edu>
2017-11-03 16:31:23 -0500

    Added a format_p_value function

--- R/supplemental_functions.R
+++ R/supplemental_functions.R
@@ -47,3 +47,24 @@ gather_factors <- function(x, cols, rev = FALSE) {
 
     return(unique(levs))
 }
+
+#' Format p-value with a specified significant digit cutoff
+#'
+#' \code{format_p_value} will take a set of p-values, round to the specified
+#' significant digits, and convert values that are less than the specified
+#' cutoff to "<'cutoff'"
+#'
+#' @param x A vector of p-values
+#' @param digits how many significant digits are to be used.
+#' @return A vector of formatted p-values.
+#' @export
+format_p_val <- function(x, digits = 3) {
+    if (is.character(x)) {
+        x <- as.numeric(x)
+    }
+    cutoff <- 10^(-digits)
+    idx <- which(x < cutoff)
+    x <- as.character(round(x, digits))
+    x[idx] <- paste0("<", cutoff)
+    return(x)
+}
