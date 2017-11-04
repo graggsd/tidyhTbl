5cec3ec72b73990f9eecab3ecba8f485362a3038
Stephen Gragg <graggsd@uab.edu>
2017-11-03 22:53:22 -0500

    Moved the format_p_val function to the sgcleanup package

--- R/supplemental_functions.R
+++ R/supplemental_functions.R
@@ -48,27 +48,6 @@ get_factors <- function(x, cols, rev = FALSE) {
     return(unique(levs))
 }
 
-#' Format p-value with a specified significant digit cutoff
-#'
-#' \code{format_p_value} will take a set of p-values, round to the specified
-#' significant digits, and convert values that are less than the specified
-#' cutoff to "<'cutoff'"
-#'
-#' @param x A vector of p-values
-#' @param digits how many significant digits are to be used.
-#' @return A vector of formatted p-values.
-#' @export
-format_p_val <- function(x, digits = 3) {
-    if (is.character(x)) {
-        x <- as.numeric(x)
-    }
-    cutoff <- 10^(-digits)
-    idx <- which(x < cutoff)
-    x <- as.character(round(x, digits))
-    x[idx] <- paste0("<", cutoff)
-    return(x)
-}
-
 #' Function to retrieve the first level from one or more columns in a data.frame
 #'
 #' \code{get_first_levels} is a function that may be used to retrieve the first
