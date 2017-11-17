adb5f5150cda67e70e295233722806a10d38ab82
Stephen Gragg <graggsd@uab.edu>
2017-11-17 01:13:25 -0600

    Added a help page for the overall package.

---
+++ R/package_documentations.R
@@ -0,0 +1,15 @@
+#' tidyhTbl: A tidy data implementation of the htmlTable package
+#'
+#' This package contains wrapper functions for the htmlTable package and
+#' accessory functions to help format tidy data.
+#'
+#' @section htmlTable wrappers:
+#' htmlTable_td
+#'
+#' @section Accessory functions:
+#' get_factor_levels
+#' get_first_levels
+#'
+#' @docType package
+#' @name tidyhTbl
+NULL
